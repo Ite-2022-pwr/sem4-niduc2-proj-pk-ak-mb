d4cccfc18ca580606656d0dd1c6aa736da37530d
arkregiel <arkregiel@gmail.com>
2024-04-06 15:47:12 +0200

    minor changes

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import utils.rng as rng
 
 if __name__ == '__main__':
     lcg = rng.LinearCongruentialGenerator()
-    for i in range(10):
+    for i in range(20):
         print(lcg.next_int())
--- utils/rng.py
+++ utils/rng.py
@@ -5,7 +5,7 @@ import time
 class LinearCongruentialGenerator:
     """Pseudorandom number generator"""
     
-    def __init__(self, a: int = 2137, c: int = 69, m: int = 2**13, seed: int = None):
+    def __init__(self, a: int = 48271, c: int = 0, m: int = 2**31 - 1, seed: int = None):
         """LCG constructor
         :param: a - the multiplier
         :param: c - the increment
@@ -15,7 +15,7 @@ class LinearCongruentialGenerator:
 
         assert 0 < m, "param m (modulus) must be a positive number"
         assert 0 < a < m, "param a (multiplier) must be 0 < a < m"
-        assert 0 < c < m, "param c (increment) must be 0 < c < m"
+        assert 0 <= c < m, "param c (increment) must be 0 <= c < m"
 
         self.__multiplier = a
         self.__increment = c
@@ -24,6 +24,7 @@ class LinearCongruentialGenerator:
         if seed is None:
             self.__x0 = int(os.getpid() + time.time())
         else:
+            assert 0 <= seed < m, "param seed must be 0 <= seed < m"
             self.__x0 = seed
 
         self.__x_next = (self.__multiplier * self.__x0 + self.__increment) % self.__modulus
